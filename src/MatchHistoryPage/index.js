5440f8ed4fe03d8253900ad436f65d1e20ec1b5b
Ethan Shen <ethanshen.wu@gmail.com>
2020-05-12 19:18:03 -0700

    refactored files to avoid circular import: Match.js, Summoner.js in backend

--- src/MatchHistoryPage/index.js
+++ src/MatchHistoryPage/index.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const API_URL = `https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/`
-const CONFIG = {
-  headers: {
-    "X-Riot-Token": "RGAPI-4f8db37e-0edb-494a-8392-67acc74d00ea",
-  }
-}
-
 export default class MatchHistoryPage extends Component {
   constructor(props) {
     super(props);
@@ -18,13 +11,13 @@ export default class MatchHistoryPage extends Component {
   }
 
   async componentDidMount() {
-    let username = 'vaikryn';
     let summoner = await axios.get(`${API_URL}${username}`, CONFIG);
     debugger
   }
 
   render() {
     return(
+      this.isLoading ? <h1>Loading</h1> :
       <div></div>
     )
   }
