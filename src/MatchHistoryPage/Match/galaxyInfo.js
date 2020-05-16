0447c0134bf980f8d696ceb748bace54bf121db0
Ethan Shen <ethanshen.wu@gmail.com>
2020-05-15 18:56:01 -0700

    added momentjs, galaxy information to MatchHistory momentjs for human readable time added galaxy names

---
+++ src/MatchHistoryPage/Match/galaxyInfo.js
@@ -0,0 +1,11 @@
+export const galaxies = {
+  TFT3_GameVariation_None: 'Normal Galaxy',
+  TFT3_GameVariation_TwoStarCarousels: 'Star Cluster',
+  TFT3_GameVariation_Bonanza: 'Treasure Trove',
+  TFT3_GameVariation_FreeNeekos: 'The Neekoverse',
+  TFT3_GameVariation_MidGameFoN: 'Superdense Galaxy',
+  TFT3_GameVariation_FourCostFirstCarousel: 'Lilac Nebula',
+  TFT3_GameVariation_FreeRerolls: 'Trade Sector',
+  TFT3_GameVariation_BigLittleLegends: 'Medium Legends',
+  TFT3_GameVariation_StartingItems: 'Galactic Armory',
+}
--- src/MatchHistoryPage/MatchHistory/index.js
+++ src/MatchHistoryPage/MatchHistory/index.js
@@ -18,15 +18,23 @@ class MatchHistory extends Component {
     return (
       <Table>
         <thead>
-          <th colSpan="3">
+          <th colSpan="4">
             <span>Recent Games</span>
           </th>
         </thead>
         <tbody>
-          {this.props.matchHistory.map(({ matchId, match }) => <Match key={matchId} info={match}/>)}
-          <tr>
-            <img src={require('../../assets/set3/traits/blademaster.png')} />
-          </tr>
+          {this.props.matchHistory.map(({ 
+            id, 
+            details, 
+            datetime, 
+            duration,
+            galaxy }) => <Match 
+          key={id} 
+          details = {details}
+          datetime = {datetime}
+          duration = {duration}
+          galaxy = {galaxy}
+          />)}
         </tbody>
       </Table>
     )
